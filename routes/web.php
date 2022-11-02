440f958ab9aa17f1964088d28e245e5ca80a48ee
Jordadn <jordan.potenciano@gmail.com>
2022-11-02 21:11:42 +0100

    push2

--- routes/web.php
+++ routes/web.php
@@ -1,18 +1,6 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-//Llamamos a todos los controladores que usaremos
-use App\Http\Controllers\UsuariosControllers;
-use App\Http\Controllers\ProductoController;
-use App\Http\Controllers\Jadde;
-use App\http\Controllers\CustomerController;
-use App\http\Controllers\OficinaController;
-use App\http\Controllers\PedidosController;
-
-
-
-
-
 
 /*
 |--------------------------------------------------------------------------
@@ -29,34 +17,8 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/usuarios', function () {
-    return "Aqui van los usuarios";
-});
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth'])->name('dashboard');
 
-Route::get('/usuarios2',
-[UsuariosController::class, 'listar']
-);
-
-Route::get('/productos',
-[ProductoController::class, 'listar']
-);
-
-Route::get('/jadde',
-[ProductoController::class, 'listar']
-);
-Route::get('/jadde',
-[Jadde::class, 'listar']
-);
-
-route::get('/clientes',
-[CustomerController::class, 'listar']
-);
-
-route::get('/oficinas',
-[OficinaController::class,'listar']
-);
-
-route::get('/pedidos',
-[PedidosController::class,'listar']
-);
-
+require __DIR__.'/auth.php';
